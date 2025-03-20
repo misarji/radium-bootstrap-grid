34f859d1d7afc88a194be04a15eb636d0d6340d1
Keshav Mishra <keshav.kr.mishra@gmail.com>
2025-03-20 17:44:23 +0530

    Updated exports

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import Container from './container.react';
 import Column from './column.react';
 import Row from './row.react';
 
-module.exports.Container = Container;
-module.exports.Column = Column;
-module.exports.Row = Row;
+exports.Container = Container;
+exports.Column = Column;
+exports.Row = Row;
