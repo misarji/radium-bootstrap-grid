7cbee0f4d25537e196450fab7db2abaabbec0e0c
Keshav Mishra <keshav.kr.mishra@gmail.com>
2025-03-20 17:09:50 +0530

    Update plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,44 @@
+const path = require('path');
+
 module.exports = {
   context: __dirname,
-  entry: "./src/index.js",
+  entry: {
+    app: './src/index.js'
+  },
   output: {
-    path: './lib',
     filename: "main.js", // no hash in main.js because index.html is a static page
+    path: path.resolve(__dirname, 'lib'),
+    clean: true,
     // chunkFilename: "[hash]/js/[id].js",
     // hotUpdateMainFilename: "[hash]/update.json",
     // hotUpdateChunkFilename: "[hash]/js/[id].update.js"
     libraryTarget: "commonjs2"
   },
   module: {
-    loaders: [
-        {
-          test: /\.js?$/,
-          exclude: /(node_modules|bower_components)/,
-          loader: 'babel-loader',
-          query: {
-            presets: ['es2015', 'stage-0', 'react'],
-            plugins: ["transform-decorators-legacy", "transform-class-properties"]
-          }
-        },{
-          test: /\.js?$/,
-          /* add a directory to exclude regex */
-          exclude: /(scripts|script|node_modules|bower_components|web)/,
-          loaders: ['eslint-loader']
-        }
-      ]
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader:
+              `babel-loader?${
+                JSON.stringify({
+                  presets: ["@babel/preset-react"],
+                  plugins: ["transform-decorators-legacy", "transform-class-properties"],
+                })}`,
+          },
+        ],
+      },{
+        test: /\.js?$/,
+        /* add a directory to exclude regex */
+        exclude: /(scripts|script|node_modules|bower_components|web)/,
+        use: [
+          {
+            loader:'eslint-loader',
+          },
+        ]
+      }
+    ]
   }
 };
